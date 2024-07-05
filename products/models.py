516c295cf7af4fa06452d599b4d4ef9198a6c677
unknown <ghaithmichel@gmail.com>
2024-07-06 00:06:16 +0300

    small adjustments on models

--- products/models.py
+++ products/models.py
@@ -32,31 +32,42 @@ class Size(models.Model):
         return self.name
 
 class Image(models.Model):
-    title = models.CharField(max_length= 100, null= True)
+    title = models.CharField(max_length= 100, null= True, blank= True)
     image = models.ImageField(upload_to = 'images')
     
     def __str__(self):
-        return self.title
+        return self.title if self.title else 'No title available'
 
 class Product(models.Model):
     name = models.CharField(max_length= 250, null= False, default= '')
-    description = models.CharField(max_length= 250, null= True)
-    quantity = models.IntegerField(null= False, default= 0)
-    price = models.IntegerField(null= False, default= 0)
+    description = models.CharField(max_length= 250, null= True, blank= True)
+    quantity = models.PositiveIntegerField(null= False, default= 0)
+    price = models.PositiveIntegerField(null= False, default= 0)
     created_at = models.DateField(null= False, default= timezone.now)
-    brand = models.ForeignKey(Brand, on_delete= models.PROTECT, null= True)
+    brand = models.ForeignKey(Brand, on_delete= models.PROTECT, null= True, blank= True)
     product_type = models.ForeignKey(Type, on_delete= models.PROTECT, null= False)
     material = models.ForeignKey(Material, on_delete= models.PROTECT, null= False)
     fit = models.ForeignKey(Fit, on_delete= models.PROTECT, null= False) 
+    
+    def __str__(self):
+        return self.name 
+    
 
 class Product_Color_Size(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     color = models.ForeignKey(Color , on_delete=models.PROTECT)
     size = models.ForeignKey(Size, on_delete=models.PROTECT)
-    quantity = models.IntegerField(default= 0, null= False)
-    price = models.IntegerField(default= 0, null= False)
+    quantity = models.PositiveIntegerField(default= 0, null= False)
+    price = models.PositiveIntegerField(default= 0, null= False)
 
+    def __str__(self):
+        return str(self.product) + " " + str(self.color) + ' ' + str(self.size)
+    
 class Product_Color(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     color = models.ForeignKey(Color , on_delete=models.PROTECT)
     image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return str(self.product) + " " + str(self.color)
+    
